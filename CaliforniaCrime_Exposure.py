cc4e480e1ef5bfc498fc01e46379782912eb8afa
ivnagpal <63216494+ivnagpal@users.noreply.github.com>
2020-08-01 21:59:09 -0700

    Commit

--- CaliforniaCrime_Exposure.py
+++ CaliforniaCrime_Exposure.py
@@ -21,10 +21,23 @@ client = gspread.authorize(credentials)
 work_sheet = client.open('California-Felony-Exposure-Dataset').get_worksheet(1)
 data = work_sheet.get_all_records()
 
+work_sheet_2 = client.open('California-Felony-Exposure-Dataset').get_worksheet(2)
+data_2 = work_sheet_2.get_all_records()
+
+
 #Create Exposure Data Frame
 exposure_df = pd.DataFrame(data)
 exposure_df['Statutory_Code'] = exposure_df['Statutory_Code'].astype(str)
 path = os.getcwd() + '/calexposure_df.csv'
 exposure_df.to_csv (path, index = False, header=True)
+
+#Create Probation Eligibility Restriction
+prob_df = pd.DataFrame(data_2)
+prob_df.replace('UE', 'Eligible only in unusual cases',inplace = True)
+prob_df.replace('NE', 'Not eligible',inplace = True)
+prob_df.replace('JC', 'Probation usually must be conditioned on Mandatory Jail Term',inplace = True)
+path2 = os.getcwd() + '/probrestriction.csv'
+prob_df.to_csv (path2, index = False, header=True)
+
 
  
